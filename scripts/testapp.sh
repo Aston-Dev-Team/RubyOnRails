5793f2fb2e8a24a522039312f3e6abb6c75a2e20
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2016-09-16 00:52:13 +0100

    Removed potentiol for provisioning to enter loop on testapp install

--- scripts/testapp.sh
+++ scripts/testapp.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+NC='\033[0m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+
 $SHELL<<EOF1
 	#echo $PWD
 
-	sudo rails new testapp #-d mysql
+	if [ -d "testapp/" ]; then
+		echo -e "${YELLOW}testapp already created${NC}"
+	else
+		sudo rails new testapp #-d mysql
+	fi
 	cd testapp/
 	sudo rails server -b 0.0.0.0 > /dev/null &
 
